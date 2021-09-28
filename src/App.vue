713bfc5dcc6b850b09c2c9d4a556723d22dfaf38
ulteerr <ulterpwnz@list.ru>
2021-09-28 10:19:18 +0300

    v1.0

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,78 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="app">
+    <h1>Страница с постами</h1>
+    <my-button
+        @click="showDialog"
+        style="margin: 15px 0"
+    >
+      Создать пост
+    </my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form
+          @create="createPost"
+      >
+      </post-form>
+    </my-dialog>
+    <post-list
+        :posts="posts"
+        @remove="removePost"
+    />
   </div>
-  <router-view/>
 </template>
 
+<script>
+import PostForm from "./components/PostForm";
+import PostList from "./components/PostList";
+import MyDialog from "./components/UI/MyDialog";
+import axios from 'axios'
+
+export default {
+  components: {
+    MyDialog,
+    PostList,
+    PostForm
+  },
+  data() {
+    return {
+      posts: [],
+      dialogVisible: false
+    }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post)
+      this.dialogVisible = false
+    },
+    removePost(post) {
+      this.posts = this.posts.filter(p => p.id !== post.id)
+    },
+    showDialog() {
+      this.dialogVisible = true
+    },
+    async fetchPost() {
+      try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+        this.posts = response.data
+      } catch (e) {
+        alert('Ошибка')
+      }
+    }
+  },
+  mounted() {
+    this.fetchPost()
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+.app {
+  padding: 20px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
