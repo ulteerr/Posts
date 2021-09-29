b76e7919c4ee3e66f468e38600d42fcbc6488c0e
ulteerr <ulterpwnz@list.ru>
2021-09-29 19:00:05 +0300

    v1.4

--- src/pages/PostPage.vue
+++ src/pages/PostPage.vue
@@ -4,6 +4,7 @@
     <my-input
         v-model="searchQuery"
         placeholder="Поиск..."
+        v-focus
     >
     </my-input>
     <div class="app_btns">
@@ -30,7 +31,7 @@
         v-if="!isPostsLoading"
     />
     <div v-else class="donut"></div>
-    <div ref="observer" class="observer"></div>
+    <div v-intersection="loadMorePosts" class="observer"></div>
     <!--    <div class="page__wrapper">-->
     <!--     <div-->
     <!--         v-for="pageNumber in totalPages"-->
@@ -127,18 +128,18 @@ export default {
   },
   mounted() {
     this.fetchPost()
-    const options = {
-      rootMargin: '0px',
-      threshold: 1.0
-    }
-
-    const callback = (entries, observer) => {
-      if (entries[0].isIntersecting && this.page < this.totalPages) {
-        this.loadMorePosts()
-      }
-    }
-    const observer = new IntersectionObserver(callback, options)
-    observer.observe(this.$refs.observer)
+    // const options = {
+    //   rootMargin: '0px',
+    //   threshold: 1.0
+    // }
+    //
+    // const callback = (entries, observer) => {
+    //   if (entries[0].isIntersecting && this.page < this.totalPages) {
+    //     this.loadMorePosts()
+    //   }
+    // }
+    // const observer = new IntersectionObserver(callback, options)
+    // observer.observe(this.$refs.observer)
   },
   computed: {
     sortedPosts() {
